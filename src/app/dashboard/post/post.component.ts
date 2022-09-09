1c938e2289040b036e7ff78eca50a45657d78316
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-09-09 14:19:42 +0800

    added blogPost lists(regular mode)

--- src/app/dashboard/post/post.component.ts
+++ src/app/dashboard/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post/post.service';
+import { BlogPost } from 'src/app/shared/blog-post';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
+  blogPosts!:BlogPost[];
+  isLoading=true;
 
-  constructor() { }
+  constructor(private blogPostService:PostService) { }
 
   ngOnInit(): void {
+    this.getBlogPosts();
+  }
+
+  getBlogPosts(){
+    this.blogPostService.getBlogPosts().subscribe((res)=>{
+      this.blogPosts=res
+      this.isLoading=false;
+    })
+
   }
 
 }

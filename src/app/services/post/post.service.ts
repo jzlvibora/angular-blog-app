ca2933bbefd5680cfad69552e8d358531e9b224f
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-09-13 19:12:27 +0800

    refactor: set variable BASE_URL

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -7,30 +7,32 @@ import { BlogPost } from 'src/app/shared/blog-post';
   providedIn: 'root'
 })
 export class PostService {
+
+  BASE_URL = 'https://6319a9566b4c78d91b403f35.mockapi.io/ngBlog/v1/blogs/';
  
   constructor(private http:HttpClient) { }
 
   public getBlogPosts():Observable<BlogPost[]>{
-    return this.http.get<BlogPost[]>('https://6319a9566b4c78d91b403f35.mockapi.io/ngBlog/v1/blogs');
+    return this.http.get<BlogPost[]>(this.BASE_URL);
   }
 
   public getBlogPost(id:number):Observable<BlogPost>{
-    return this.http.get<BlogPost>(`https://6319a9566b4c78d91b403f35.mockapi.io/ngBlog/v1/blogs/${id}`);
+    return this.http.get<BlogPost>(`${this.BASE_URL}${id}`);
   }
 
   public postBlogPost(blogPost:BlogPost){
-    return this.http.post('https://6319a9566b4c78d91b403f35.mockapi.io/ngBlog/v1/blogs', blogPost);
+    return this.http.post(this.BASE_URL, blogPost);
   }
 
   public postBlogPosts(blogPosts:BlogPost[]){
-    return this.http.post('https://6319a9566b4c78d91b403f35.mockapi.io/ngBlog/v1/blogs', blogPosts);
+    return this.http.post(this.BASE_URL, blogPosts);
   }
 
   public updateBlogPost(blogPost:BlogPost, id:number){
-    return this.http.put(`https://6319a9566b4c78d91b403f35.mockapi.io/ngBlog/v1/blogs/${id}`, blogPost);
+    return this.http.put(`${this.BASE_URL}${id}`, blogPost);
   }
 
   public deleteBlogPost( id:number){
-    return this.http.delete(`https://6319a9566b4c78d91b403f35.mockapi.io/ngBlog/v1/blogs/${id}`);
+    return this.http.delete(`${this.BASE_URL}${id}`);
   }
 }

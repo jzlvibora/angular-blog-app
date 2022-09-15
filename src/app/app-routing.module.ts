421570bfa4d14e95518b30743402506344dec89b
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-09-15 13:59:17 +0800

    implemented admin lazy laoding 1.ng g m admin --routing 2.removed route paths for admin in app-routing and move to admin-routing 3.declare components under admin module in declarations including truncate pipe. imported material,forms,reactiveforms modules 4.added admin lazy loaded routes in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddPostComponent } from './admin/add-post/add-post.component';
-import { AdminComponent } from './admin/admin.component';
-import { EditPostComponent } from './admin/edit-post/edit-post.component';
-import { PostListComponent } from './admin/post-list/post-list.component';
 import { PostComponent } from './post/post.component';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
 import { LoginLayoutComponent } from './login-layout/login-layout.component';
@@ -19,14 +15,8 @@ const routes: Routes = [
 },
 {path:'', component:HomeLayoutComponent,
 children:[
-  {path:'', redirectTo:'admin', pathMatch:'full'},
-  {path:'admin', component:AdminComponent, 
-   children:[
-    {path:'', component:PostListComponent},
-    {path:'new', component:AddPostComponent},
-    {path:'edit/:id', component:EditPostComponent}
-   ]},
-   {path:'posts', component:PostComponent}
+  {path: '',loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
+  {path:'posts', component:PostComponent}
 ]}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { PostComponent } from './post/post.component';
-import { AdminComponent } from './admin/admin.component';
 import { TruncatePipe } from './shared/pipes/truncate.pipe';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './shared/material/material.module';
-import { AddPostComponent } from './admin/add-post/add-post.component';
 import { HttpClientModule } from '@angular/common/http';
-import { EditPostComponent } from './admin/edit-post/edit-post.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PostListComponent } from './admin/post-list/post-list.component';
 import { DatePipe } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { LoginLayoutComponent } from './login-layout/login-layout.component';
@@ -29,12 +25,8 @@ import { HomeLayoutComponent } from './home-layout/home-layout.component';
     AppComponent,
     NavigationComponent,
     PostComponent,
-    AdminComponent,
     TruncatePipe,
     FooterComponent,
-    AddPostComponent,
-    EditPostComponent,
-    PostListComponent,
     PageNotFoundComponent,
     LoginComponent,
     LoginLayoutComponent,

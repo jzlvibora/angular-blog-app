c934038a82379194fba55078769e66626ac5e5ea
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-09-23 16:15:42 +0800

    added initial login ui and functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm!:FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm=this.buildForm();
+  }
+
+  private buildForm():FormGroup{
+    return new FormGroup({
+      username:new FormControl(null,[
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      password:new FormControl(null,[
+        Validators.required,
+
+      ])
+    })
+  }
+
+  get f(){
+    return this.loginForm.controls;
+  }
+
+  onSubmit(){
+    if(this.loginForm.invalid){
+      console.log('invalid');
+      return
+    }
+    console.log('lalalal',this.loginForm.controls)
+
   }
 
 }

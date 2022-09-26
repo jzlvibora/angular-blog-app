dd0d7538857bb6dff74bf92e472a92de39ae29b7
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-09-26 17:09:10 +0800

    moved login component inside auth folder

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm!:FormGroup;
+  isSignedin=false;
+
+  constructor(private authService:AuthService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.isSignedin=this.authService.isUserSignedin();
+    if(this.isSignedin){
+      this.router.navigateByUrl('posts')
+    }
+    this.loginForm=this.buildForm();
+  }
+
+  private buildForm():FormGroup{
+    return new FormGroup({
+      username:new FormControl(null,[
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      userpwd:new FormControl(null,[
+        Validators.required,
+
+      ])
+    })
+  }
+
+  get f(){
+    return this.loginForm.controls;
+  }
+
+  onSubmit(){
+    if(this.loginForm.invalid){
+      console.log('invalid');
+      return
+    }
+    this.authService.signin(this.loginForm.value).subscribe((res)=>{
+      this.router.navigateByUrl('posts')
+    },)
+
+  }
+
+}

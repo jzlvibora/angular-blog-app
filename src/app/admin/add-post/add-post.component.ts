3105a03794402dc5f31e0544f56f2c34b2bcff6e
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-09-28 15:32:30 +0800

    added tag and likes field

--- src/app/admin/add-post/add-post.component.ts
+++ src/app/admin/add-post/add-post.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PostService } from 'src/app/services/post/post.service';
+import { BlogPost } from 'src/app/shared/blog-post';
+import { PostRequest } from 'src/app/shared/post-request';
 import { AlertsService } from '../alerts.service';
 
 @Component({
@@ -14,6 +16,15 @@ export class AddPostComponent implements OnInit {
   form!:FormGroup;
   error:string|null=null;
   isSubmitSuccessful:boolean=false;
+  postRequest: PostRequest={
+    body: '',
+    image: '',
+    // createdAt: '',
+    likes: 0,
+    tag: {
+      id: 0
+    }
+  };
 
   constructor(private router:Router,private blogPostService:PostService, private alertService:AlertsService) { }
 
@@ -24,10 +35,12 @@ export class AddPostComponent implements OnInit {
   private buildForm():FormGroup{
     return new FormGroup({
       title: new FormControl(null,[Validators.required]),
-      author: new FormControl(null,[Validators.required]),
+      // author: new FormControl(null,[Validators.required]),
       image:new FormControl(null,[Validators.required]),
       body: new FormControl(null, [Validators.required]),
-      createdAt:new FormControl(Date.now().toLocaleString(), [Validators.required])
+      likes:new FormControl(null,[Validators.required]),
+      createdAt:new FormControl(Date.now().toLocaleString(), [Validators.required]),
+      tagId:new FormControl(null,[Validators.required])
     })
   }
 
@@ -36,13 +49,25 @@ export class AddPostComponent implements OnInit {
   }
 
   onSubmit(){
+    console.log(this.form.value)
+    // this.blogPost.author=this.f.author;
+    // console.log(this.f['author'].value)
+    // this.blogPost.author=this.f['author'].value;
+    // this.postRequest.title==this.f['title'].value;
+    // this.postRequest.tag.id==this.f['tagId'].value;
+    // this.postRequest.image==this.f['image'].value;
+    // this.postRequest.body==this.f['body'].value;
+    // this.postRequest.likes==this.f['likes'].value;
+    // this.postRequest.createdAt==this.f['createdAt'].value;
+
+
     if(this.form.invalid){
       this.alertService.infoNotification();
       return
     }
-    this.blogPostService.postBlogPost(this.form.value).subscribe((res)=>{
+    this.blogPostService.postBlogPost({title:this.f['title'].value, image:this.f['image'].value, body:this.f['body'].value, likes:this.f['likes'].value, tag: { id: this.f['tagId'].value} }).subscribe((res)=>{
       console.log(res);
-      this.alertService.successNotification();
+      
       this.form.reset();
     },(err:HttpErrorResponse)=>{
       this.error=err.message;

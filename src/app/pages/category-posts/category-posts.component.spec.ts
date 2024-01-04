e3d4e36c7bb1fc3d1f8d801c191a77711cbd8aab
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2024-01-04 23:10:23 +0800

    category posts page component

---
+++ src/app/pages/category-posts/category-posts.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CategoryPostsComponent } from './category-posts.component';
+
+describe('CategoryPostsComponent', () => {
+  let component: CategoryPostsComponent;
+  let fixture: ComponentFixture<CategoryPostsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CategoryPostsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CategoryPostsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/category-posts/category-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-posts',
+  templateUrl: './category-posts.component.html',
+  styleUrls: ['./category-posts.component.scss']
+})
+export class CategoryPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
